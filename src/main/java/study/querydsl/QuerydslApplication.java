47ad9b8c1170faf8e311d6c103e57d61b8f6c6fd
LSJeong <lsj930224@naver.com>
2023-05-26 13:05:33 +0900

    Querydsl

---
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -0,0 +1,18 @@
+package study.querydsl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@SpringBootApplication
+public class QuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslApplication.class, args);
+	}
+
+}
